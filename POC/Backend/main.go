347835c390de827b00f00d6542f51b8eab148a6e
LixiosDelios <alexandre.guillaud@epitech.eu>
2025-11-19 13:52:47 +0100

    feat(poc/backend): implement basic user CRUD operations with Gin framework

---
+++ POC/Backend/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"Backend/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	controllers.InitDataBase()
+	r := gin.Default()
+
+	r.GET("/users", controllers.GetUsers)
+	r.POST("/users", controllers.AddUser)
+	r.DELETE("/users/:id", controllers.DeleteUser)
+	r.PUT("/users/:id", controllers.UpdateUser)
+	err := r.Run()
+	if err != nil {
+		return
+	}
+}
