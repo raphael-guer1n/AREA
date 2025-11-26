5221304c312e68be615e7a381e67443a569338fc
raphael <raphael.guerin@epitech.eu>
2025-11-26 16:10:41 +0100

    feat(Mobile): add basic page skeleton

---
+++ Mobile/area_mobile/lib/theme/app_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+final ThemeData appTheme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+  useMaterial3: true,
+  textTheme: const TextTheme(
+    bodyMedium: TextStyle(fontSize: 16),
+  ),
+);
