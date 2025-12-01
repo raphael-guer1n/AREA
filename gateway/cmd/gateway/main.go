ed8a46ecf2c72b5966596f3d3eb7c10441a54c42
Alexis <alexis.constantinopoulos@epitech.eu>
2025-12-01 13:21:22 +0100

    feat(gateway): implement main function and service configuration setup

--- gateway/cmd/gateway/main.go
+++ gateway/cmd/gateway/main.go
@@ -1,4 +1,22 @@
-package main
+		package main
 
-func main() {
-}
+		import (
+			"fmt"
+			"github.com/raphael-guer1n/AREA/area-gateway/internal/config"
+		)
+
+		func main() {
+			c := config.ServiceConfig{
+				Name: "test",
+				BaseURL: "http://localhost:3000",
+				Routes: []config.RouteConfig{
+					{
+						Path: "/ping",
+						Methods: []string{"GET"},
+						AuthRequired: false,
+					},
+				},
+			}
+
+			fmt.Printf("%+v\n", c)
+		}
