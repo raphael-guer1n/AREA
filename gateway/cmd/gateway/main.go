98e87a8b00321b69f5ae2ad6cd52121df0f6dd9e
Alexis <alexis.constantinopoulos@epitech.eu>
2025-12-02 10:40:30 +0100

    feat(registry): initialize and load services in main function

--- gateway/cmd/gateway/main.go
+++ gateway/cmd/gateway/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/raphael-guer1n/AREA/area-gateway/internal/config"
+	"github.com/raphael-guer1n/AREA/area-gateway/internal/registry"
 )
 
 func main() {
@@ -16,4 +17,12 @@ func main() {
 	}
 
 	fmt.Println("All configs are valid.")
+	reg := registry.NewRegistry()
+	if err := reg.Load(services); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Loaded %d services\n", len(reg.ListServices()))
+	fmt.Printf("Loaded %d routes\n", len(reg.ListAllRoutes()))
+
 }
