15281da3411f51867276e077955715f977af6145
Alexis <alexis.constantinopoulos@epitech.eu>
2025-12-03 13:34:00 +0100

    feat(router): update reverse proxy to include query parameters and set error handler

--- gateway/internal/router/proxy.go
+++ gateway/internal/router/proxy.go
@@ -17,7 +17,16 @@ func NewReverseProxy(baseURL string) http.Handler {
 	originalDirector := proxy.Director
 	proxy.Director = func(req *http.Request) {
 		originalDirector(req)
+
+		req.Header.Set("X-Forwarded-Host", req.Host)
+		req.Header.Set("X-Forwarded-Proto", req.URL.Scheme)
+		req.Header.Set("X-Forwarded-For", req.RemoteAddr)
+
 		req.Host = target.Host
+	}
+
+	proxy.ErrorHandler = func(w http.ResponseWriter, r *http.Request, err error) {
+		http.Error(w, "Bad Gateway", http.StatusBadGateway)
 	}
 
 	return proxy
