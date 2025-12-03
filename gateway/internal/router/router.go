e90d0efc7d558c6569bab493ddc0d5574a27b4e6
Alexis <alexis.constantinopoulos@epitech.eu>
2025-12-03 11:36:54 +0100

    feat(router): implement Router struct and Build method to manage routes

--- gateway/internal/router/router.go
+++ gateway/internal/router/router.go
@@ -1,1 +1,49 @@
 package router
+
+import (
+	"net/http"
+
+	"github.com/raphael-guer1n/AREA/area-gateway/internal/config"
+	"github.com/raphael-guer1n/AREA/area-gateway/internal/middleware"
+	"github.com/raphael-guer1n/AREA/area-gateway/internal/registry"
+)
+
+type Router struct {
+	registry   *registry.Registry
+	config     *config.GatewayConfig
+	authMW     *middleware.AuthMiddleware
+	permMW     *middleware.PermissionsMiddleware
+	internalMW *middleware.InternalMiddleware
+	loggingMW  *middleware.LoggingMiddleware
+	mux        *http.ServeMux
+}
+
+func NewRouter(
+	reg *registry.Registry,
+	cfg *config.GatewayConfig,
+	auth *middleware.AuthMiddleware,
+	perm *middleware.PermissionsMiddleware,
+	internal *middleware.InternalMiddleware,
+	logging *middleware.LoggingMiddleware,
+) *Router {
+	return &Router{
+		registry:   reg,
+		config:     cfg,
+		authMW:     auth,
+		permMW:     perm,
+		internalMW: internal,
+		loggingMW:  logging,
+		mux:        http.NewServeMux(),
+	}
+}
+
+func (rt *Router) Build() (*http.ServeMux, error) {
+
+	routes := rt.registry.ListAllRoutes()
+
+	for _, route := range routes {
+		_ = route
+	}
+
+	return rt.mux, nil
+}
