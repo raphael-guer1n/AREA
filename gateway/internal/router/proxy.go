20014eb47e81c167866c00def714fab459d0a9d5
Alexis <alexis.constantinopoulos@epitech.eu>
2025-12-03 16:07:23 +0100

    feat(router): enhance reverse proxy to sanitize sensitive headers and append forwarded headers

--- gateway/internal/router/proxy.go
+++ gateway/internal/router/proxy.go
@@ -1,9 +1,11 @@
 package router
 
 import (
+	"net"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"strings"
 )
 
 func NewReverseProxy(baseURL string) http.Handler {
@@ -14,20 +16,59 @@ func NewReverseProxy(baseURL string) http.Handler {
 
 	proxy := httputil.NewSingleHostReverseProxy(target)
 
-	originalDirector := proxy.Director
 	proxy.Director = func(req *http.Request) {
-		originalDirector(req)
+		clientIP := clientIPFromRemoteAddr(req.RemoteAddr)
 
-		req.Header.Set("X-Forwarded-Host", req.Host)
-		req.Header.Set("X-Forwarded-Proto", req.URL.Scheme)
-		req.Header.Set("X-Forwarded-For", req.RemoteAddr)
+		appendForwardedHeader(req, "X-Forwarded-For", clientIP)
+		appendForwardedHeader(req, "X-Forwarded-Host", req.Host)
+		appendForwardedHeader(req, "X-Forwarded-Proto", schemeFromRequest(req))
 
+		req.Header.Set("X-Real-IP", clientIP)
+		req.Header.Del("X-Internal-Secret")
+		req.URL.Scheme = target.Scheme
+		req.URL.Host = target.Host
 		req.Host = target.Host
-	}
 
+		basePath := strings.TrimSuffix(target.Path, "/")
+		incomingPath := req.URL.Path
+		if !strings.HasPrefix(incomingPath, "/") {
+			incomingPath = "/" + incomingPath
+		}
+		req.URL.Path = basePath + incomingPath
+	}
 	proxy.ErrorHandler = func(w http.ResponseWriter, r *http.Request, err error) {
 		http.Error(w, "Bad Gateway", http.StatusBadGateway)
 	}
 
 	return proxy
+}
+
+func clientIPFromRemoteAddr(addr string) string {
+	if strings.HasPrefix(addr, "[") {
+		end := strings.Index(addr, "]")
+		if end != -1 {
+			return addr[1:end]
+		}
+	}
+
+	ip, _, err := net.SplitHostPort(addr)
+	if err == nil {
+		return ip
+	}
+	return addr
+}
+
+func appendForwardedHeader(req *http.Request, header string, value string) {
+	if prev := req.Header.Get(header); prev != "" {
+		req.Header.Set(header, prev+", "+value)
+	} else {
+		req.Header.Set(header, value)
+	}
+}
+
+func schemeFromRequest(req *http.Request) string {
+	if req.TLS != nil {
+		return "https"
+	}
+	return "http"
 }
